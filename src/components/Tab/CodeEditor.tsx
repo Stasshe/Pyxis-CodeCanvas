998b4fb95ecb0ee47b3d3b59822b25c540470f87
Stasshe <egnm9stasshe@gmail.com>
2025-08-15 09:05:09 +0000

    ts

--- src/components/Tab/CodeEditor.tsx
+++ src/components/Tab/CodeEditor.tsx
@@ -286,7 +286,7 @@ export default function CodeEditor({
       esModuleInterop: true,
       jsx: monaco.languages.typescript.JsxEmit.React,
       reactNamespace: 'React',
-      allowJs: true,
+      allowJs: false,
       typeRoots: ['node_modules/@types']
     });
 
