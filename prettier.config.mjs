1e3e9579b07dc1bff8db5f028d742a4ee848030a
Stasshe <egnm9stasshe@gmail.com>
2025-10-01 04:46:03 +0000

    Prettier設定を新規作成し、既存の設定ファイルを削除

---
+++ prettier.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  quoteProps: 'consistent',
+  proseWrap: 'always',
+  singleAttributePerLine: true,
+  embeddedLanguageFormatting: 'auto',
+};
+
+export default config;
