138c53c13cc1429324e06046ec9492d9f7ba03b9
Stasshe <egnm9stasshe@gmail.com>
2025-11-05 13:50:47 +0000

    feat: Unixコマンドのエラーハンドリングを強化し、存在しないコマンドに対するテストを追加

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/jest.setup.js'],
   testMatch: ['**/tests/*.test.ts?(x)'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
