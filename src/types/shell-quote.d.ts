c9e61649eda32c756b0f38bf12db1a920c3617c7
Stasshe <egnm9stasshe@gmail.com>
2026-01-05 07:55:58 +0900

    Delete src/types/shell-quote.d.ts

--- src/types/shell-quote.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'shell-quote' {
-  /** Parse a command string into an array of arguments */
-  export function parse(cmd: string, env?: Record<string, string>): (string | { op: string })[];
-  /** Quote an array of arguments into a command string */
-  export function quote(args: string[]): string;
-}
